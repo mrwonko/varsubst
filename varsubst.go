32d70d3089625eccec8b9146d61174fa5ce594d6
Willi Schinmeyer <willi.schinmeyer@freiheit.com>
2016-11-10 17:50:16 +0100

    replace complicated handwritten parser with simple regexp substitution

--- varsubst.go
+++ varsubst.go
@@ -3,131 +3,28 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
+	"regexp"
 )
 
-/*
-  <letter>   = A-Z
-  <digit>    = 0-9
-  <special>  = "_"
-  <varname>  = <letter> {<letter> | <digit> | <special>}
-  <variable> = <dollar> <lparen> <varname> <rparen>
-  <dollar>   = "$"
-  <lparen>   = "("
-  <rparen>   = ")"
-*/
+var envVarRegexp = regexp.MustCompile(`\$\{[A-Z_0-9]+\}`)
 
-var eof = rune(0)
-
-func isLetter(ch rune) bool {
-	return (ch >= 'A' && ch <= 'Z')
-}
-func isDigit(ch rune) bool {
-	return (ch >= '0' && ch <= '9')
-}
-func isSpecial(ch rune) bool {
-	return ch == '_'
-}
-func isDollar(ch rune) bool {
-	return ch == '$'
-}
-func isLparen(ch rune) bool {
-	return ch == '{'
-}
-func isRparen(ch rune) bool {
-	return ch == '}'
-}
-
-func read(r *bufio.Reader) rune {
-	ch, _, err := r.ReadRune()
+func Scan(reader io.Reader) string {
+	input, err := ioutil.ReadAll(reader)
 	if err != nil {
-		return eof
+		return ""
 	}
-	return ch
-}
-
-func unread(r *bufio.Reader) { _ = r.UnreadRune() }
-
-func varname(r *bufio.Reader) string {
-	var literal, varname string
-	var ch rune
-
-	ch = read(r)
-	literal += string(ch)
-	if !isLetter(ch) {
-		return literal
-	} else {
-		varname += string(ch)
-	}
-
-	for {
-		ch = read(r)
-		literal += string(ch)
-		if isLetter(ch) || isDigit(ch) || isSpecial(ch) {
-			varname += string(ch)
-		} else {
-			if isRparen(ch) {
-				unread(r)
-				return varname
-			} else {
-				return literal
-			}
+	return envVarRegexp.ReplaceAllStringFunc(string(input), func(placeholder string) string {
+		name := placeholder[2 : len(placeholder)-1] // drop $( and )
+		if varvalue := os.Getenv(name); varvalue != "" {
+			return varvalue
 		}
-	}
-}
-
-func variable(r *bufio.Reader) string {
-	var literal, varvalue string
-	var ch rune
-
-	// read "$("
-	ch = read(r)
-	literal += string(ch)
-	if !isDollar(ch) {
-		return literal
-	}
-	ch = read(r)
-	literal += string(ch)
-	if !isLparen(ch) {
-		return literal
-	}
-
-	// read <varname>
-	name := varname(r)
-	literal += name
-
-	// read env var with this name
-	if varvalue = os.Getenv(name); varvalue == "" {
-		return literal
-	}
-	// read ")"
-	ch = read(r)
-	literal += string(ch)
-	if !isRparen(ch) {
-		return literal
-	}
-	return varvalue
-}
-
-func Scan(r *bufio.Reader) string {
-	var result string
-	for {
-		if ch := read(r); ch != eof {
-			if isDollar(ch) {
-				unread(r)
-				result += variable(r)
-			} else {
-				result += string(ch)
-			}
-		} else {
-			break
-		}
-	}
-	return result
+		return placeholder
+	})
 }
 
 func main() {
-	r := bufio.NewReader(os.Stdin)
-	result := Scan(r)
-	fmt.Print(result)
+	fmt.Print(Scan(bufio.NewReader(os.Stdin)))
 }
